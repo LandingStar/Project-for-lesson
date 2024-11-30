57ba4e8e20856f88d5f8af5677cc808b2b358547
LandingStar <zhangliyang@westlake.edu.cn>
2024-11-30 15:00:38 +0800

    refine on code, fix a logic error in prevous code, add a new class set that store the space of state, next will make it multiple-thread

--- maze game/epsilon-greedy/classes.py
+++ maze game/epsilon-greedy/classes.py
@@ -2,13 +2,46 @@ import numpy as np
 import random
 
 from typing import Callable
-
+global space
+space=[]
 class block:
 	def __init__(self,actions:list):
+		self.position=0
 		self.state_value=0
 		self.actions=actions
 		self.action_values=np.array([0]*len(actions))
-
+class set():
+	def __init__(self,blocks:list,*maxlen):
+		self.blocks=list(blocks)
+		self.maxlen=list(maxlen)
+		prod=1
+		for i in maxlen:
+			prod*=i
+		self.length=prod
+		cnt=0
+		for i in self.blocks:
+			i.postion=cnt
+			cnt+=1
+	def __len__(self):
+		return self.length
+	def __iter__(self):
+		return self.blocks.__iter__()
+	def __getitem__(self,index):
+		return self.blocks[index]
+	def getitem(self,*keys):
+		position=0
+		base=1
+		for i in range(len(self.maxlen)-1,-1,-1):
+			position+=keys[i]*base
+			base*=self.maxlen[i]
+		return self.blocks[position]
+	def get_index(self,*keys):
+		position=0
+		base=1
+		for i in range(len(self.maxlen)-1,-1,-1):
+			postion+=keys[i]*base
+			base*=self.maxlen[i]
+		return position
 class action:
 	def __init__(self,next_state:list,next_state_p:np.ndarray):
 		self.action_value=0
@@ -19,7 +52,7 @@ class action:
 		#self.next_state_p_expected=np.array([1/len(next_state)]*len(next_state)) #the expected position after some action used for questions that the next state is not settled after a certain action
 	def __iter__(self):
 		return self
-	def __next__(self)->block: #giving next state by taking this action
+	def __next__(self)->int: #giving next state by taking this action
 		total=np.sum(self.next_state_p)
 		x=random.random()*total
 		for i in range(len(self.next_state_p)):
@@ -28,7 +61,6 @@ class action:
 				return self.next_state[i]
 		return self.next_state[0]
 	def update(self,ret,a:Callable[[int,],float]=lambda x:1/x): # 'a' is the conerge coiffiecient in RM
-		
 		self.action_value=self.action_value-a(self.visit_cnt)*(self.action_value-ret)
 		self.visit_cnt+=1
 		
@@ -50,8 +82,8 @@ class policy:
 		for i in range(len(probability)):
 			x-=probability[i]
 			if x<=0:
-				return state.actions[i]
-		return self.next_state[0]
+				return i
+		return 0
 
 class episode:
 	def __init__(self,state:block,policy:policy,step:int):# use a initial state to create an episode with lenth of 'step' by the policy
@@ -59,10 +91,10 @@ class episode:
 		self.now_state=state
 		self.policy=policy
 		for i in range(step):
-			a=policy.choice(self.now_state)
+			a=policy.choice(space[self.now_state])
 			#a=self.act(policy)
 			self.track.append((self.now_state,a))
-			self.now_state=next(a)
+			self.now_state=next(space[self.now_state].actions[a])
 	def __iter__(self)->list:
 		return self.track
 	def act(self,policy:policy)->action:				# return the action the policy choose  ,same as choice in class policy
--- maze game/epsilon-greedy/test.py
+++ maze game/epsilon-greedy/test.py
@@ -1,20 +1,23 @@
 import numpy as np
 import args
-from classes import *
+import classes
+import sys
 import threading
+import random
 max_column=0
 max_row=0
+sys.stdout=open("out.txt","w")
 def import_mazz(path:str)->list:
     global max_column,max_row
     file=open(path,"r")
     n,m=tuple(map(lambda x:int(x),file.readline()[:-1].split(",")[:2]))
     max_row,max_column=n,m
-    blocks=[block([]) for i in range(m)for j in range(n)]
+    blocks=[classes.block([]) for i in range(m)for j in range(n)]
     value=[]
     def give_actions(pos:int)->list:
         r,c=[pos//m,pos%m]
         a=[-1,0,1]
-        ret=[action(np.array([blocks[(r+i)%n*m+(c+j)%m],]),np.array([1,]))for i in a for j in a]
+        ret=[classes.action(np.array([(r+i)%n*m+(c+j)%m,]),np.array([1,]))for i in a for j in a]
         cnt=0
         for i in a:
             for j in a:
@@ -28,26 +31,28 @@ def import_mazz(path:str)->list:
         value+=tuple(map(lambda x:np.float64(x),lin))
     for i in range(n*m):
         blocks[i].actions=give_actions(i)
-    return blocks
-blocks=import_mazz("C:\\Users\\16329\\Source\\Repos\\LandingStar\\CST-Project\\maze game\\epsilon-greedy\\mazz.txt")
+    classes.space=classes.set(blocks,n,m)
+    classes.space.maxlen=[n,m]
+    return classes.space
+classes.space=import_mazz("C:\\Users\\16329\\Source\\Repos\\LandingStar\\CST-Project\\maze game\\epsilon-greedy\\mazz.txt")
 for cnt_round in range(1500):
-    epsilon_policy=policy(args.epsilon_greedy(100/(150+cnt_round**2)))
-    init_state=blocks[random.randint(0,len(blocks)-1)]
-    eps=episode(init_state,epsilon_policy,args.episode_lenth)
+    epsilon_policy=classes.policy(args.epsilon_greedy(100/(100+cnt_round)))
+    init_state=random.randint(0,len(classes.space)-1)
+    eps=classes.episode(init_state,epsilon_policy,args.episode_lenth)
     sample_return=0
     for s,a in eps.track[::-1]:
-        sample_return=a.action_reward+args.gamma*sample_return
-        a.update(sample_return)
+        sample_return=classes.space[s].actions[a].action_reward+args.gamma*sample_return
+        classes.space[s].actions[a].update(sample_return)
 while 0:
     init_position=input("beginning state").split(" ")[:2]
-    init_state=blocks[init_position[0]*max_column+init_position[1]]
-    eps=episode(args.epsilon_greedy(0),10)
+    init_state=classes.space[init_position[0]*max_column+init_position[1]]
+    eps=classes.episode(args.epsilon_greedy(0),10)
     for i,j in eps.track:
         1
-state_value=tuple(map(lambda x:max(tuple(map(lambda y:y.action_value,x.actions))),blocks))
+state_value=tuple(map(lambda x:max(tuple(map(lambda y:y.action_value,x.actions))),classes.space.blocks))
 from sys import stdout
-for i in range(len(blocks)):
+for i in range(len(classes.space)):
     if not i%max_column:
         stdout.write("\n")
-    stdout.write(f"{state_value[i]:5.4f}\t")
+    stdout.write(f"{state_value[i]:5.3f}\t")
 stdout.write("\n")
