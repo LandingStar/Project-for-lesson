38ff5d709710294d176c49b2b281bdfe7239ee20
LandingStar <zhangliyang@westlake.edu.cn>
2024-12-05 23:57:28 +0800

    add the visualize function. a small modify turn it into off policy, which successfully make it clever. Next we may consider to  involve the deep network into it.

--- maze game/epsilon-greedy/args.py
+++ maze game/epsilon-greedy/args.py
@@ -1,11 +1,16 @@
 import numpy as np
 from typing import Callable
-episode_lenth=250   #the lenth of 
-gamma=0.92
+import random
+episode_lenth=120   #the lenth of 
+gamma=0.9
 convergence_critirion=0.01
 reward_common_road=0
 reward_forbidden_area=-1
 reward_target=10
+def random_choice(lenth):
+    def the_policy(action_values):
+        return np.array([1/lenth]*lenth)
+    return the_policy
 def epsilon_greedy(epsilon:float)->Callable[[np.ndarray,],np.ndarray]: #give a function giving the probability choosing actions under epsilon-greedy policy.Used in <class policy>.__init__
     def the_policy(action_values:np.ndarray):
         cnt=len(action_values)
